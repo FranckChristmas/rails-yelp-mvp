c91fad1fcb2c473107a3e6b04c7ec82a32493a48
francois <francois.noell@gmail.com>
2024-11-20 16:11:07 +0800

    far advanced  ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+# 2. Create the instances 🏗️
+puts "Creating restaurants..."
+Restaurant.create!(name: "Shikoku", address: "7 Boundary St, London E2 7JE", phone_number: "+33684245558", category: "japanese")
+puts "Created #{Restaurant.name} "
+Restaurant.create!(name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "+3390853095823", category: "italian")
+puts "Created #{Restaurant.name} "
+
+# 3. Display a message 🎉
+puts "Finished! Created #{Restaurant.count} restaurants."
